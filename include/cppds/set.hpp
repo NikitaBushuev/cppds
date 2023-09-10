e58787cdfbe375046dcfaf503be946ade1c65576
NikitaBushuev <nikitabushuev.personal@gmail.com>
2023-09-10 21:30:08 +0600

    Add Doxygen comments to "include/cppds/set.hpp"

--- include/cppds/set.hpp
+++ include/cppds/set.hpp
@@ -1,3 +1,8 @@
+/**
+ * @file set.hpp
+ * @brief A custom set implementation using open addressing for collision resolution.
+ */
+
 #pragma once
 
 #include <cstring>
@@ -5,47 +10,94 @@
 #include <initializer_list>
 #include <stdexcept>
 
-#include "hash.hpp"
+#include "hash.hpp" // Include necessary header(s)
 
 namespace cppds {
+    /**
+     * @brief A custom set data structure.
+     *
+     * This set uses open addressing for collision resolution and supports basic operations
+     * like insert, erase, contains, clear, size, and empty.
+     *
+     * @tparam _Tp The type of elements stored in the set.
+     */
     template <typename _Tp>
     class set {
     public:
+        // Type aliases for clarity
         using key_type = _Tp;
         using value_type = _Tp;
         using size_type = std::size_t;
 
+        /**
+         * @brief Default constructor for the set.
+         */
         set() = default;
 
+        /**
+         * @brief Constructor to initialize the set from an array.
+         *
+         * @param _array An array of values to initialize the set with.
+         * @tparam N The size of the array.
+         */
         template <size_type N>
         set(value_type (&_array)[N]) {
             operator=(_array);
         }
 
+        /**
+         * @brief Constructor to initialize the set from an initializer list.
+         *
+         * @param _list An initializer list of values to initialize the set with.
+         */
         set(const std::initializer_list<value_type> &_list) {
             operator=(_list);
         }
 
+        /**
+         * @brief Assignment operator to assign values from an array.
+         *
+         * @param _array An array of values to assign to the set.
+         * @tparam N The size of the array.
+         * @return A reference to the modified set.
+         */
         template <size_type N>
         set &operator=(value_type (&_array)[N]) {
+            this->clear();
             for (const value_type &value: _array) {
                 this->insert(value);
             }
             return *this;
         }
 
+        /**
+         * @brief Assignment operator to assign values from an initializer list.
+         *
+         * @param _list An initializer list of values to assign to the set.
+         * @return A reference to the modified set.
+         */
         set &operator=(const std::initializer_list<value_type> &_list) {
+            this->clear();
             for (const value_type &value: _list) {
                 this->insert(value);
             }
             return *this;
         }
 
+        /**
+         * @brief Destructor to clean up the set.
+         */
         ~set() {
             this->clear();
         }
 
+        /**
+         * @brief Insert a value into the set.
+         *
+         * @param _value The value to insert.
+         */
         void insert(const value_type &_value) {
+            // Calculate hash using a custom hash function
             size_type hash = __fnv1hash(&_value, sizeof(_value));
 
         try_again:
@@ -71,8 +123,14 @@ namespace cppds {
             this->_M_hdata[idx] = hash;
         }
 
-        void erase(const value_type &_value) {
-            size_type hash = __fnv1hash(&_value, sizeof(_value));
+        /**
+         * @brief Erase a value from the set.
+         *
+         * @param _key The value to erase.
+         */
+        void erase(const key_type &_key) {
+            // Calculate hash using a custom hash function
+            size_type hash = __fnv1hash(&_key, sizeof(_key));
 
             size_t idx = this->capacity() ? hash % this->capacity() : 0;
 
@@ -88,7 +146,14 @@ namespace cppds {
             }
         }
 
+        /**
+         * @brief Check if a value exists in the set.
+         *
+         * @param _key The value to check for.
+         * @return `true` if the value exists in the set, `false` otherwise.
+         */
         bool contains(const key_type &_key) const {
+            // Calculate hash using a custom hash function
             size_type hash = __fnv1hash(&_key, sizeof(_key));
 
             size_t idx = this->capacity() ? hash % this->capacity() : 0;
@@ -102,6 +167,9 @@ namespace cppds {
             return idx < this->capacity() && this->_M_hdata[idx] == hash;
         }
 
+        /**
+         * @brief Clear the set, removing all elements.
+         */
         void clear() {
             for (size_type i = 0; i < this->capacity(); ++i) {
                 if (this->_M_hdata[i]) {
@@ -118,6 +186,11 @@ namespace cppds {
             this->_M_vdata = nullptr;
         }
 
+        /**
+         * @brief Get the size of the set.
+         *
+         * @return The number of elements in the set.
+         */
         size_type size() const {
             size_type size = 0;
 
@@ -130,15 +203,30 @@ namespace cppds {
             return size;
         }
 
+        /**
+         * @brief Check if the set is empty.
+         *
+         * @return `true` if the set is empty, `false` otherwise.
+         */
         bool empty() const {
             return this->size() == 0;
         }
 
     protected:
+        /**
+         * @brief Get the current capacity of the set.
+         *
+         * @return The current capacity of the set.
+         */
         size_type capacity() const {
             return this->_M_capacity;
         }
 
+        /**
+         * @brief Reserve space for a specified capacity.
+         *
+         * @param _capacity The new capacity to reserve.
+         */
         void reserve(size_type _capacity) {
             if (_capacity < this->capacity()) {
                 return;
@@ -165,8 +253,8 @@ namespace cppds {
             }
         }
 
-        size_type *_M_hdata {};
-        value_type *_M_vdata {};
-        size_type _M_capacity {};
+        size_type *_M_hdata {}; // Array to store hash values
+        value_type *_M_vdata {}; // Array to store values
+        size_type _M_capacity {}; // Current capacity of the set
     };
 }
